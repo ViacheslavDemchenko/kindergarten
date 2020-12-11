0897abfbd1297cadbb5c4cb0009fd76177dedd51
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2020-12-11 14:15:53 +0300

    Finished third section desktop

--- wp-content/themes/twentytwenty-child/js/main.js
+++ wp-content/themes/twentytwenty-child/js/main.js
@@ -1,5 +1,29 @@
 "use strict";
 
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 21,
+  slidesPerView: 4,
+  loop: true,
+  freeMode: true,
+  loopedSlides: 5,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 22,
+  slidesPerView: 2,
+  loop: true,
+  loopedSlides: 5,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+"use strict";
+
 /*!
  * @copyright Copyright (c) 2017 IcoMoon.io
  * @license   Licensed under MIT license
