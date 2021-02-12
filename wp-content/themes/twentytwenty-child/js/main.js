d8fc157d517aebdeab02518f868ba922a9b96671
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2021-02-12 12:24:13 +0300

    Added phone mask header input

--- wp-content/themes/twentytwenty-child/js/main.js
+++ wp-content/themes/twentytwenty-child/js/main.js
@@ -173,6 +173,60 @@
 ;
 
 (function () {
+  //Задаем инпут
+  var input = document.getElementById('header-form__phone'); //Функция маски инпута
+
+  function setMask(event) {
+    //Задаем в переменную нажатую клавишу
+    var pressedKey; //Условие, проверяющее нажатую клавишу
+
+    event.keyCode && pressedKey === event.keyCode; //Устанавливаем возможность ввода цифр только с последней позиции  
+
+    input.setSelectionRange(input.value.length, input.value.length); //Задаем внешний вид маски инпута
+
+    var maskType = '+7 (___) ___-__-__',
+        i = 0,
+        //Проверка и замена value инпута по буквенно
+    replaceValue = maskType.replace(/\D/g, ''),
+        prevValue = this.value.replace(/\D/g, ''),
+        currentValue = maskType.replace(/[_\d]/g, function (a) {
+      return i < prevValue.length ? prevValue.charAt(i++) || replaceValue.charAt(i) : a;
+    }); //Защита от стирания первых двух цифр (+7)            
+
+    i = currentValue.indexOf('_');
+
+    if (i != -1) {
+      i < 5 && (i = 3);
+      currentValue = currentValue.slice(0, i);
+    } //Регулярное выражение для проверки value инпута        
+
+
+    var reg = maskType.substr(0, this.value.length).replace(/_+/g, function (a) {
+      return '\\d{1,' + a.length + '}';
+    }).replace(/[+()]/g, '\\$&');
+    reg = new RegExp('^' + reg + '$'); //Проверка содержимого инпута на регулярное выражение, длинну и нажимаемые клавиши
+
+    if (!reg.test(this.value) || this.value.length < 5 || pressedKey > 47 && pressedKey < 58) {
+      this.value = currentValue;
+    } else if (event.type === 'blur' && this.value.length < 5) {
+      this.value = '';
+    } // Устанавливаем курсор в конец строки в инпуте, если при нажатии кнопки он стоит не в конце
+
+
+    input.setSelectionRange(input.value.length, input.value.length);
+  } //Запуск функции setMask через обработчик событий
+
+
+  input.addEventListener('input', setMask, false);
+  input.addEventListener('focus', setMask, false);
+  input.addEventListener('blur', setMask, false);
+  input.addEventListener('keydown', setMask, false);
+})();
+"use strict";
+
+;
+
+(function () {
   var btnExcursionHead = document.querySelector('.header-top__btn');
   var popupExcursion = document.querySelector('.popup-excursion');
   var popupCall = document.querySelector('.popup-call');
